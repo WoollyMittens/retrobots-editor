d676197ffd89cf6ac2356d31e5f2648f6212beb0
Maurice van Creij <maurice@sydneyhikingtrips.com>
2025-08-01 07:32:31 +1000

    Add boiler plate for preview controls

--- sequences.js
+++ sequences.js
@@ -3,9 +3,9 @@ export class Sequences {
 		// store the model
 		this.model = model;
         // set up storage for a bank of sequences
-		this.bank = [];
+		this.bank = {};
 		this.step = 0;
-		this.active = 0;
+		this.active = null;
 		// build the interface if desired
 		this.redraw();
     }
@@ -13,7 +13,23 @@ export class Sequences {
 	redraw() {
         // if the bank requires an interface
         if (this.model.container) {
-
+            // clear the container
+            //this.model.container.innerHTML = '';
+			// create the blank selector
+			// create the selector list
+				// create the selector radio
+				// create the name field
+				// create the frames field
+				// create the remove button
+			// create the add button
 		}
 	}
+	// add a new sequence to the list
+	add() {}
+	// remove a sequence from the list
+	remove(name) {}
+	// rename a sequence
+	rename(name, input) {}
+	// update a sequence
+	update(name, input) {}
 }
